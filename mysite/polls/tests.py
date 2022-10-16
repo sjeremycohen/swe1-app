63bd5faa7c01c164eae57b11700045d6b81d8dc0
sjeremycohen <sjeremycohen@gmail.com>
2022-10-16 13:38:37 -0400

    Blackened and added eb deploy credentials for travis

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -5,8 +5,8 @@ from polls.models import Question, Choice
 class QuestionTest(TestCase):
     def setUp(self):
         self.q = Question.objects.create(question_text="Is this a successful test?")
-        #Choice.objects.create(question=q.pk, choice_text="Yes")
-        #Choice.objects.create(question=q.pk, choice_text="Also Yes")
+        # Choice.objects.create(question=q.pk, choice_text="Yes")
+        # Choice.objects.create(question=q.pk, choice_text="Also Yes")
 
     def test_question(self):
         question = Question.objects.get(id=self.q.pk)
